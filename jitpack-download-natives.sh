e3a4dda367de6a652ab7a061b3d303b1a2f7656a
JCM <jacama2009@gmail.com>
2021-03-01 12:25:22 +0100

    Fix typo

--- jitpack-download-natives.sh
+++ jitpack-download-natives.sh
@@ -10,5 +10,5 @@ curl -L -o src/main/resources/native/windows/amd64/discord_game_sdk_jni.dll \
 	https://github.com/JnCrMx/discord-game-sdk4j/releases/download/$version/windows-amd64-discord_game_sdk_jni.dll
 curl -L -o src/main/resources/native/windows/x86/discord_game_sdk_jni.dll \
 	https://github.com/JnCrMx/discord-game-sdk4j/releases/download/$version/windows-x86-discord_game_sdk_jni.dll
-curl -L -o src/main/resources/native/linux/amd64libdiscord_game_sdk_jni.so \
+curl -L -o src/main/resources/native/linux/amd64/libdiscord_game_sdk_jni.so \
 	https://github.com/JnCrMx/discord-game-sdk4j/releases/download/$version/linux-amd64-libdiscord_game_sdk_jni.so
