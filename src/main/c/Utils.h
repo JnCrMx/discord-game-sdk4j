102e3e61b26fea521f509005ae185dce63cbf986
JCM <jcm@jcm.re>
2022-12-14 11:25:54 +0100

    Remove native code that is no longer needed

--- src/main/c/Utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <stdlib.h>
-#include <discord_game_sdk.h>
-
-jobject create_java_relationship(JNIEnv *env, struct DiscordRelationship relationship);
-
-#endif
