e1bb55cb8294ec66dceb49cd109a445cc48b9476
Torben <git@letorbi.com>
2022-12-19 00:58:08 +0100

    Add non-blocking mode to WindowsDiscordChannel

--- src/main/java/de/jcm/discordgamesdk/impl/channel/WindowsDiscordChannel.java
+++ src/main/java/de/jcm/discordgamesdk/impl/channel/WindowsDiscordChannel.java
@@ -7,6 +7,7 @@ import java.nio.channels.FileChannel;
 
 public class WindowsDiscordChannel implements DiscordChannel {
     private final FileChannel channel;
+    private boolean blocking = true;
 
     public WindowsDiscordChannel() throws IOException {
         RandomAccessFile raf = new RandomAccessFile("\\\\?\\pipe\\discord-ipc-0", "rw");
@@ -18,14 +19,30 @@ public class WindowsDiscordChannel implements DiscordChannel {
     }
 
     public void configureBlocking(boolean block) throws IOException {
-        // nothing to do here
+        blocking = block;
     }
 
     public int read(ByteBuffer dst) throws IOException {
+        if (!blocking && (channel.size() - channel.position()) < dst.remaining())
+        {
+            return 0;
+        }
         return channel.read(dst);
     }
 
     public long read(ByteBuffer[] dsts, int offset, int length) throws IOException {
+        long remaining = 0;
+        if (!blocking)
+        {
+            for (int i = offset; i < offset+length; i++)
+            {
+                remaining += dsts[i].remaining();
+            }
+            if ((channel.size() - channel.position()) < remaining)
+            {
+                return 0;
+            }
+        }
         return channel.read(dsts, offset, length);
     }
 
