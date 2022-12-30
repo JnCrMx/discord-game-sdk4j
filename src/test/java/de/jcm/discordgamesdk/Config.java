1780f946c5d0bf45588b19016f700716c65e5a96
Torben <git@letorbi.com>
2022-12-30 19:07:30 +0100

    Clean up test config

--- src/test/java/de/jcm/discordgamesdk/Config.java
+++ src/test/java/de/jcm/discordgamesdk/Config.java
@@ -1,9 +1,7 @@
 package de.jcm.discordgamesdk;
 
 public class Config {
-    public static final long CLIENT_ID = 698611073133051974L;
-    public static final long USER_ID = 628649350851198996L; // letorbi
-    //public static final long USER_ID = 352386023159758848L; // local Discord user
-    public static final long RELATIONSHIP_ID = 1055075131761889351L; // letorbi2
-    //public static final long RELATIONSHIP_ID = 691614879399936078L; // befriended Discord user
+	public static final long CLIENT_ID = 698611073133051974L;
+	public static final long USER_ID = 352386023159758848L; // local Discord user
+	public static final long RELATIONSHIP_ID = 691614879399936078L; // befriended Discord user
 }
