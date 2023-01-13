2fd19c3f9d4180332334cd01004870e8f2c53dcd
Torben <git@letorbi.com>
2023-01-13 14:33:40 +0100

    Ensure that JitPack uses Java 16

---
+++ jitpack-openjdk-16.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Installs OpenJDK 16 via SDKMAN!
+# https://leaves.one/2021/08/07/publish-package-on-jitpack-with-java16/
+
+JV=`java -version 2>&1 >/dev/null | head -1`
+echo $JV | sed -E 's/^.*version "([^".]*)\.[^"]*".*$/\1/'
+
+if [ "$JV" != 16 ]; then
+	case "$1" in
+	install)
+		echo "Installing SDKMAN..."
+		curl -s "https://get.sdkman.io" | bash
+		source ~/.sdkman/bin/sdkman-init.sh
+		sdk version
+		sdk install java 16.0.2-open
+		;;
+	use)
+		echo "must source ~/.sdkman/bin/sdkman-init.sh"
+		exit 1
+		;;
+	esac
+fi
