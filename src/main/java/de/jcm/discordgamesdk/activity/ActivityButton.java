84ec512cc7e8046af7cb4500967d5b13451b5b2c
Gravita <12893402+gravit0@users.noreply.github.com>
2023-08-24 00:56:04 +0700

    Add ActivityButton docs

--- src/main/java/de/jcm/discordgamesdk/activity/ActivityButton.java
+++ src/main/java/de/jcm/discordgamesdk/activity/ActivityButton.java
@@ -13,23 +13,44 @@ public class ActivityButton {
     public ActivityButton() {
     }
 
+    /**
+     * Construct a new ActivityButton
+     * @param label The text displayed on the button
+     * @param url Link that is clicked to
+     */
     public ActivityButton(String label, String url) {
         this.label = label;
         this.url = url;
     }
 
+    /**
+     * Get a label
+     * @return button label
+     */
     public String getLabel() {
         return label;
     }
 
+    /**
+     * Set a label
+     * @param label The text displayed on the button
+     */
     public void setLabel(String label) {
         this.label = label;
     }
 
+    /**
+     * Get a url
+     * @return button url
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Set a url
+     * @param url Link that is clicked to
+     */
     public void setUrl(String url) {
         this.url = url;
     }
