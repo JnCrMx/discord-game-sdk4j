35c5ddcc5d17b09cb3fa513ce9e7cd4a9ac21e5d
JCM <jcm@jcm.re>
2025-01-10 13:51:48 +0100

    Update documentation and include some more helper functions

--- src/main/java/de/jcm/discordgamesdk/activity/ActivityTimestamps.java
+++ src/main/java/de/jcm/discordgamesdk/activity/ActivityTimestamps.java
@@ -14,7 +14,9 @@ public class ActivityTimestamps
 
 	/**
 	 * <p>Sets the time the user started playing.</p>
-	 * <p>This causes an "elapsed" duration to show up.</p>
+	 * <p>If the end timestamp ({@link ActivityTimestamps#setEnd}) is set as well,
+	 * this cause a progress indicator to show up.</p>
+	 * <p>Otherwise, this causes an "elapsed" duration to show up.</p>
 	 * @param start Start time
 	 */
 	public void setStart(Instant start)
@@ -33,7 +35,9 @@ public class ActivityTimestamps
 
 	/**
 	 * <p>Sets the time the user will be done.</p>
-	 * <p>This causes an "remaining" duration to show up.</p>
+	 * <p>If the start timestamp ({@link ActivityTimestamps#setStart}) is set as well,
+	 * this cause a progress indicator to show up.</p>
+	 * <p>This causes a "remaining" duration to show up.</p>
 	 * @param end End time
 	 */
 	public void setEnd(Instant end)
@@ -48,5 +52,42 @@ public class ActivityTimestamps
 	public Instant getEnd()
 	{
 		return Instant.ofEpochSecond(end);
+	}
+
+	/**
+	 * <p>Sets both start and end time of the activity simultaneously.</p>
+	 * <p>This causes a progress indicator to show up.</p>
+	 * <p>This method exists purely for convenience and is equivalent to calling {@link ActivityTimestamps#setStart}
+	 * and {@link ActivityTimestamps#setEnd} individually.</p>
+	 * @param start Start time
+	 * @param end End time
+	 */
+	public void setStartAndEnd(Instant start, Instant end) {
+		this.start = start.getEpochSecond();
+		this.end = end.getEpochSecond();
+	}
+
+	/**
+	 * Clears the start time.
+	 */
+	public void clearStart() {
+		this.start = null;
+	}
+
+	/**
+	 * Clears the end time.
+	 */
+	public void clearEnd() {
+		this.end = null;
+	}
+
+	/**
+	 * <p>Clears both start and end time simultaneously.</p>
+	 * <p>This method exists purely for convenience and is equivalent to calling {@link ActivityTimestamps#clearStart}
+	 * and {@link ActivityTimestamps#clearEnd} individually.</p>
+	 */
+	public void clear() {
+		this.start = null;
+		this.end = null;
 	}
 }
